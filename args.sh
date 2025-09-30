376d60f3c3541f8c685120257e783bb419f3ab09
nikitha84 <nikitha84640@gmail.com>
2025-09-30 22:12:32 +0530

    Args

---
+++ args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2 : Hello $PERSON1"
+echo "$PERSON1 : Good monrning $PERSON2 "
+echo "$PERSON2 : very good morning $PERSON1"
