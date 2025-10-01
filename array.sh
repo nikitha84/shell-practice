f84d1e0a7a2497724ac6b44eec422f647bd5b14a
nikitha84 <nikitha84640@gmail.com>
2025-10-01 11:20:22 +0530

    Array

--- array.sh
+++ array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FRUITS=("Apple" "Banana" "Grep")
-echo "First value is: {$FRUITS[0]}"
-echo "First value is: {$FRUITS[1]}"
-echo "First value is: {$FRUITS[2]}"
+FRUITS=("Apple" "Banana" "Grap")
+echo "First value is: ${FRUITS[0]}"
+echo "First value is: ${FRUITS[1]}"
+echo "First value is: ${FRUITS[2]}"
 #echo "ALL Fruit is : {$FRUITES[@]}"
