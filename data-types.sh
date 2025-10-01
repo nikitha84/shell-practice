c85b56f961cf8ccf1c122a271c71dbeb7fe9e81a
nikitha84 <nikitha84640@gmail.com>
2025-10-01 11:02:09 +0530

    data-types

---
+++ data-types.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$((NUMBER+$NUMBER))"
+echo "Total: $SUM"
--- user-variable-2.sh
+++ user-variable-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "please enter your username"
 read -s USERNAME
-# echo "username is :$USERNAME"
+
 echo "please enter your password"
 read -s PASSWORD
