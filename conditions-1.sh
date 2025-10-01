329634555d7eab529c02a45b2969e83ef6a21dfd
nikitha84 <nikitha84640@gmail.com>
2025-10-01 12:08:59 +0530

    conditions

--- conditions-1.sh
+++ conditions-1.sh
@@ -4,9 +4,19 @@ ID=$(id -u)
 if [ $ID -ne 0 ];
 then
     echo "ERROR:: Please run with root user"
+    exit 1
 else
     echo "You rea root user"
 fi
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR: Installing Mysql failed"
+    exist 1
+else
+    echo "Installing Mysql suscess"
+fi   
+
 
 
