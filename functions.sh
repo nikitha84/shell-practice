fbb5012936816902deb7312f6611d5403d53bfef
nikitha84 <nikitha84640@gmail.com>
2025-10-01 12:29:11 +0530

    functions

---
+++ functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ID=$(id -u)
+VALIDATE (){
+    if [ $? -ne 0 ]
+then
+    echo "ERROR: Installing  failed"
+else
+    echo "installing  sucess"
+fi
+}
+
+if [$ID -ne 0 ]
+then
+   echo "ERROE: please run with root user"
+   exit 1
+else
+    echo "you are root user"
+fi
+yum install myql -y
+VALIDATE $? 
+
+yum install git -y
+VALIDATE $?
+   
