dae34000b55371d439df7903f4c0b2bc8a953df8
nikitha84 <nikitha84640@gmail.com>
2025-10-02 15:57:55 +0530

    deleting log files

--- delete-old-log.sh
+++ delete-old-log.sh
@@ -4,17 +4,18 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
-SOURCE_DIR="/tmp/shellscript-logs"
-
-if [ ! -d $SOURCE_DIR ];     #! = Negates the condition. -d directory
+SOURCE_DIR="/tmp/shell-script-logs"
+if [ ! -d $SOURCE_DIR ]
 then
-    echo -e "$R source directory: $SOURCE_DIR does not exist $N"
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+ else
+    echo -e "$G Source directory exists $N"
 fi
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
-
+FILE_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 while IFS= read -r line
-do 
+do
     echo "Deleting file: $line"
     rm -rf $line
 done <<< $FILES_TO_DELETE
