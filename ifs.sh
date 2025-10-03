b76e4868e0fec34dc3ee77ac7b477be84b225ed7
nikitha84 <nikitha84640@gmail.com>
2025-10-03 14:45:15 +0530

    IFS

--- ifs.sh
+++ ifs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 file=/etc/passwd
 
-if[ ! -f $file ];
+if [ ! -f $file ]
 then
 echo "$file not exist"
 fi
 
-while ifs= ":" read -r username password user_id group_id user_fullname
+while ifs=":" read -r username password user_id group_id user_fullname
 do
     echo "username : $username"
-    echo "user_id: $user_id
+    echo "user_id: $user_id"
 done < $file    
