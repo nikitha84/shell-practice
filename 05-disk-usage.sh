0d355c408f704dd9626a161ceaa0ae41c669988c
nikitha84 <nikitha84640@gmail.com>
2026-02-20 14:37:12 +0530

    disk usage

---
+++ 05-disk-usage.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2
+IP_ADDRESS=$(curl -s curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+MESSAGE=""
+
+while IFS= read -r line
+do
+    USAGE=$(echo  $line | aws '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | aws '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+    #echo "Hight usage on $PARTITION : $USAGE"
+    MESSAGE+="Hight usage on $PARTITION : $USAGE % <br>" 
+done <<< $DISK_USAGE
+
+echo -e "Message body=$MESSAGE"
+
+sh 06-mail.sh "nikitha84640@gmail.com" "High disk usage Alert" "High disk usage" "$MESSAGE" "$IP_ADDRESS"
+
+#rbpvtvximyzqugky=app tocken
+
+# {
+# echo "To: devopsteam84640@gmail.com"
+# echo "Subject: Mail alert fron devopsteam"
+# echo "Content-Type: text/html"
+# echo ""
+# echo "this is test email"
+# } | msmtp "devopsteam84640@gmail.com"
+
+#curl http://checkip.amazonaws.com
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE_BODY=$4
+# IP_ADDRESS=$5
---
+++ 06-mail.sh
@@ -0,0 +1,31 @@
+TO_ADDRESS=$1
+SUBJECT=$2
+ALERT_TYPE=$3
+MESSAGE_BODY=$4
+IP_ADDRESS=$5
+TO_TEAM=$6
+
+FINAL_BODY=$(echo -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$FORMATTED_BODY/g" template.html)
+{
+echo "To: $TO_ADDRESS"
+echo "Subject: $SUBJECT"
+echo "Content-Type: $ALERT_TYPE"
+echo ""
+echo "$FINAL_BODY"
+} | msmtp "$TO_ADDRESS"
+
+
+Hi, TO_TEAM #(Devops team)
+There is an ALERT_TYPE in the system IP_ADDRESS. Please find the detailes below
+
+MESSAGE
+Regards,
+Monitoring team
+
+# {
+# echo "To: $TO_ADDRESS #devopsteam84640@gmail.com"
+# echo "Subject: Mail alert fron devopsteam"
+# echo "Content-Type: text/html"
+# echo ""
+# echo "this is test email"
+# } | msmtp "devopsteam84640@gmail.com"
